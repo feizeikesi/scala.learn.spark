84351d8005e135e5f333345f2173926288488dc2
yanglei <yanglei@tidebuy.net>
2016-06-29 23:01:17 +0800

    no message

--- src/main/scala/examples/book/learning/spark/SimplePairRDD.scala
+++ src/main/scala/examples/book/learning/spark/SimplePairRDD.scala
@@ -39,5 +39,38 @@ object SimplePairRDD {
     //返回一个根据键排序的RDD
     paris.sortByKey().collect()//Array((1,2), (3,4), (3,6))
 
+    //针对两个pair RDD 的转化操作
+    val other=sc.parallelize(List((3,9)))
+
+    paris.subtract(other).collect()
+
+    //对两个RDD进行内链接
+    paris.join(other).collect()//Array((3,(4,9)), (3,(6,9)))
+
+    //对两个RDD进行连接操作，确保第二个RDD的键必须存在
+    paris.rightOuterJoin(other).collect()//Array((3,(Some(4),9)), (3,(Some(6),9)))
+
+    //对两个RDD进行连接操作，确保第一个RDD的键必须存在
+    paris.leftOuterJoin(other).collect()// Array((1,(2,None)), (3,(4,Some(9))), (3,(6,Some(9))))
+
+    //将两个RDD中拥有相同键的数据分组到一起
+    paris.cogroup(other).collect()// Array((1,(CompactBuffer(2),CompactBuffer())), (3,(CompactBuffer(4, 6),CompactBuffer(9))))
+
+
+    //统计数字个数
+    paris.flatMap(x=>List(x._1,x._2))
+      .map(x=>(x,1)).reduceByKey((x,y)=>x+y).collect()// Array((4,1), (6,1), (2,1), (1,1), (3,2))
+
+    //统计每个key的平均值
+    paris.mapValues(x=>(x,1)).
+      reduceByKey((x,y)=>(x._1+y._1,x._2+y._2),1).
+      map(x=>(x._1,x._2._1/x._2._2)).collect() //Array((1,2), (3,5))
+
+    paris.combineByKey(
+      (v)=>(v,1),
+      (acc:(Int,Int),v)=>(acc._1+v,acc._2+1),
+      (acc1:(Int,Int),acc2:(Int,Int))=>(acc1._1+acc2._1,acc1._2+acc2._2)
+    ).map(x=>(x._1,x._2._1/x._2._2)).collect()//Array((1,2), (3,5))
+
   }
 }
