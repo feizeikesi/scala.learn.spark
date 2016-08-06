8713859f26fd607ddc78c86ee52c29586a34d782
unknown <>
2016-08-07 00:10:33 +0800

    no message

--- src/main/scala/examples/book/learning/spark/SimplePairRDD.scala
+++ src/main/scala/examples/book/learning/spark/SimplePairRDD.scala
@@ -9,13 +9,12 @@ object SimplePairRDD {
   def main (args : Array[String]) {
     val conf=new SparkConf().setAppName("")
     val sc=new SparkContext(conf)
-    val lines =sc.parallelize(List("1 2","3 4","3 6"))
+      val lines =sc.parallelize(List("1 2","3 4","3 6"))
     //创建pair RDD
     val paris=lines.map(line=>{
       val values=line.split(" ")
       (values(0).toInt,values(1).toInt)
     })
-
     paris.collect() //Array((1,2), (3,4), (3,6))
 
     //合并具有相同键的值
